4e2cdfeb9596651eaedc3d21f4badf5df58e0b37
lzkostina <kostina@umich.edu>
2025-09-17 17:35:36 -0400

    Refactor filter_subjects_with_two_timepoints with improved docstring

--- src/pipeline/load.py
+++ src/pipeline/load.py
@@ -1,7 +1,16 @@
 import pandas as pd
 
 def filter_subjects_with_two_timepoints(df: pd.DataFrame) -> pd.DataFrame:
-    """Keep only subjects with both baseline and follow-up data."""
+    """
+    Keep only subjects that have both baseline and follow-up data.
+    Parameters:
+        df: pd.DataFrame
+        Input dataframe with "Subject" column.
+    Returns:
+        pd.DataFrame:
+        Filtered dataframe which is the subset of the input dataframe
+        containing only subjects with more than one record (i.e.) 2 time points.
+    """
 
     subject_counts = df['Subject'].value_counts()
     subjects_to_keep = subject_counts[subject_counts > 1].index
